7266001abdec9f3eb90d7ee5f49c8a5d2289abfc
Boris Krasko <b.krasko@omtech.pl>
2022-06-21 15:06:01 +0200

    feat: done 04

---
+++ 04/js/scripts.js
@@ -0,0 +1,19 @@
+var Person = Backbone.Model.extend({});
+
+var person1 = new Person({
+    name: "John",
+    age: 33,
+    hobbies: ["sport", "IT"]
+});
+
+var person2 = new Person({
+    name: "Thomas",
+    age: 40,
+    hobbies: ["golf", "cars"]
+});
+
+var person3 = new Person({
+    name: "Alice",
+    age: 25,
+    hobbies: ["swimming"]
+});
